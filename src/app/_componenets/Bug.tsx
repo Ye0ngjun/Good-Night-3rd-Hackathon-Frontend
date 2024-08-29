61a8a2c985e84c11829fa51274139128282ba86b
Yeongjun Joo <wndudwns6824@naver.com>
2024-08-29 15:27:55 +0900

    design : 자잘한 수정 사항 퍼블리싱

--- src/app/_componenets/Bug.tsx
+++ src/app/_componenets/Bug.tsx
@@ -5,7 +5,7 @@ export default function Fruit() {
   return (
     <Link
       className="flex flex-col items-center justify-center gap-3 mb-10"
-      href="/openbug"
+      href="/wish-fruit"
     >
       <Image src={bug} alt="bug" className="w-48 h-48" />
       <p className="text-xl font-semibold">로또 1등 당첨!</p>
